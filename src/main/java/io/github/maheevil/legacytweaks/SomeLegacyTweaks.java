1b77b7c0ee5f153878ba8130377d89da82cbb5b4
Maheesha <maheeshasithum@gmail.com>
2022-01-10 18:00:06 +0530

    The basic setup

--- src/main/java/io/github/maheevil/legacytweaks/SomeLegacyTweaks.java
+++ src/main/java/io/github/maheevil/legacytweaks/SomeLegacyTweaks.java
@@ -1,13 +1,13 @@
-package org.example.example;
+package io.github.maheevil.legacytweaks;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Entrypoint
+public class SomeLegacyTweaks
 {
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public static void initialize() {
+    public static void TellWitnessesThatIWasInitialized() {
         LOGGER.info("Initializing");
     }
 }
